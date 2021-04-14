d892854bd2026c1b707f656492f452a282541d56
holjas <holly.jasiura@gmail.com>
2021-04-14 12:06:12 -0400

    didn't mean for main

--- src/Musings.js
+++ src/Musings.js
@@ -1,101 +1,137 @@
-// import firebase from "./firebase";
+import { useState } from "react";
+import firebase from "./firebase";
 import {
   faBookmark,
   faEdit,
   faSmile,
   faTrashAlt,
 } from "@fortawesome/free-regular-svg-icons";
-
+import { faBookmark as faBookmarkFull } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function Musings(props) {
-  // // remove musing
-  // const removeMusing = (props) => {
-  //   const userId = `-MY23fP9h4gvOjL_I7-0`;
-  //   firebase.database().ref("musings/" + userId);
-  //   // dbRef.("musings").remove();
-  //   console.log('clicked!')
-  // }
-
-  // console.log(props.musingState[1].musing[0]);
-  // props.musingState.map((x) => console.log(x.musing[0]));
-
+  const [bookmarkIcon, setBookmarkIcon] = useState(false);
+  //remove/delete musing
+  const handleRemoveMusing = (e) => {
+    const targetKey = e.currentTarget.id;
+    firebase
+      .database()
+      .ref("musings/" + targetKey)
+      .remove();
+  };
+  //edit musing, opens the textarea window to make changes
   const handleEdit = (e) => {
-    // const userId = `-MY23fP9h4gvOjL_I7-0`;
-    // firebase
-    //   .database()
-    //   .ref("musings/" + userId)
-    //   .set({
-    //     1: "this is one",
-    //     2: "TRY TO TARGET THIS",
-    //     3: "this is three",
-    //   });
-    // console.log(e.currentTarget.id);
-    // const targetedId = e.currentTarget.id;
-    const targetedId = e.currentTarget.parentNode.parentNode.children[1]; //target the musing
-    targetedId.style.display = "none"; //hide the musing
-    // console.log(e.currentTarget.parentNode.parentNode.children);
-    // targetedId.style.color = "red";
-    const editForm = e.currentTarget.parentNode.parentNode.children[2]; //target the edit box
+    const targetedId = e.currentTarget.parentNode.parentNode.children[1];
+    targetedId.style.display = "none";
+    const editForm = e.currentTarget.parentNode.parentNode.children[2];
     editForm.style.display = "flex";
-
-    // console.log(e.currentTarget.parentNode.parentNode.children[1]);
+  };
+  //edit musing save button/ send changes to firebse
+  const handleSaveEdit = (itemKey, prompt, dateTime, editMusing, event) => {
+    firebase
+      .database()
+      .ref("musings/" + itemKey)
+      .set({
+        0: prompt,
+        1: editMusing,
+        2: dateTime,
+      });
+    handleThis(event);
+  };
+  //hide the edit textform and retun to the regular card view
+  const handleThis = (e) => {
+    const targetedId = e.currentTarget.parentNode.parentNode.children[1];
+    targetedId.style.display = "-webkit-box";
+    const editForm = e.currentTarget.parentNode.parentNode.children[2];
+    editForm.style.display = "none";
   };
 
-  // const openTextBox = (area) => {
-  //   const textBoxArea = document.getElementById("textBoxArea");
-  // };
-
   return (
-    <section className="musingContainer warpperThick">
-      {props.musingState.map((item) => {
-        return (
-          <div className="musingCard" key={item.key}>
-            <div className="musingHeadline">
-              <h4>{item.musing[3]}</h4>
-            </div>
-
-            <div className="musingTextBox">
-              <p className="textEmphasis">{item.musing[1]}</p>
-              <p id="textBoxDisplay">{item.musing[2]}</p>
-              {/*  */}
-              {/*  */}
-              <form action="submit" id="textBoxEdit" className="textBoxEdit">
-                <label htmlFor="editMusing" className="visually-hidden">
-                  edit musing here
-                </label>
-                <textarea
-                  type="text"
-                  id="editMusing"
-                  // // onChange={handleChange}
-                  value={item.musing[2]}
-                />
-
-                <button>save changes</button>
-              </form>
-              {/*  */}
-              {/*  */}
-              <div className="musingNav">
-                <FontAwesomeIcon icon={faSmile} className="navMood" />
-
-                <div onClick={handleEdit} id={item.key}>
-                  <FontAwesomeIcon icon={faEdit} className="navButton" />
+    <section className="wrapper wrapperThick">
+      {/* ternary for if there are musings to show: then populate w/ musings */}
+      {props.musingState.length > 0 ? (
+        <div className="musingContainer">
+          {props.musingState.map((item) => {
+            const dateTime = item.musing[2];
+            const writingPrompt = item.musing[0];
+            const userMusing = item.musing[1];
+            return (
+              <div className="musingCard" key={item.key}>
+                <div className="musingHeadline">
+                  {/* displays time */}
+                  <h4>{dateTime}</h4>
                 </div>
-                <div>
-                  <FontAwesomeIcon
-                    icon={faTrashAlt}
-                    className="navButton"
-                    onClick={() => props.removeMusing(props.musings)}
-                  />
-                </div>
-                <div>
-                  <FontAwesomeIcon icon={faBookmark} className="navButton" />
+
+                <div className="musingTextBox">
+                  {/* displays writing prompt */}
+                  <p>{writingPrompt}</p>
+                  {/* displays previously entered musing */}
+                  <p>{userMusing}</p>
+
+                  {/*form is hidden and will appear will 'edit' it selected */}
+                  <form action="submit" id={item.key} className="textBoxEdit">
+                    <label htmlFor="editMusing" className="visually-hidden">
+                      edit musing here
+                    </label>
+                    <textarea
+                      type="text"
+                      id="editMusing"
+                      defaultValue={userMusing}
+                    />
+
+                    <button
+                      onClick={(e) => {
+                        e.preventDefault();
+                        handleSaveEdit(
+                          item.key,
+                          writingPrompt,
+                          dateTime,
+                          e.currentTarget.parentNode[0].value,
+                          e
+                        );
+                      }}
+                    >
+                      save changes
+                    </button>
+                  </form>
+                  {/*  */}
+                  {/* card nav bar, with edit/delete/bookmark */}
+                  <div className="musingNav">
+                    <FontAwesomeIcon icon={faSmile} className="navMood" />
+                    <div onClick={handleEdit} id={item.key}>
+                      <FontAwesomeIcon icon={faEdit} className="navButton" />
+                    </div>
+                    <div onClick={handleRemoveMusing} id={item.key}>
+                      <FontAwesomeIcon
+                        icon={faTrashAlt}
+                        className="navButton"
+                      />
+                    </div>
+                    <div>
+                      <FontAwesomeIcon
+                        onClick={() => setBookmarkIcon(!bookmarkIcon)}
+                        icon={bookmarkIcon ? faBookmarkFull : faBookmark}
+                        className="navButton"
+                      />
+                    </div>
+                  </div>
                 </div>
               </div>
-            </div>
+            );
+          })}
+        </div>
+      ) : (
+        // ternary for if there are NO musings to show: then retun "awaiting your musings"
+        <>
+          <div className="musingWaitingContainer">
+            <img
+              src="/assets/gwynniferWaiting.gif"
+              alt="thought bubble animation"
+            />
           </div>
-        );
-      })}
+          <h2 className="musingAwaiting">Awaiting your thoughts</h2>
+        </>
+      )}
     </section>
   );
 }
