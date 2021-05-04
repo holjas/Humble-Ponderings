6f3b321b48c2e62b48f12a2449f194f092842609
holjas <holly.jasiura@gmail.com>
2021-05-03 21:03:55 -0400

    cleaning up

--- src/Prompts.js
+++ src/Prompts.js
@@ -2,13 +2,14 @@ import React from "react";
 import firebase from "./firebase";
 import { useState, useEffect } from "react";
 
+import Moodbar from "./Moodbar";
+
 function Prompts(props) {
   const [displayPrompts, setDisplayPrompts] = useState("");
   const [userInput, setUserInput] = useState("");
   const [mood, setMood] = useState("");
 
   //set the first prompt with a random selection
-
   useEffect(() => {
     const promptLength = props.prompts.length;
     setDisplayPrompts(props.prompts[randomNumber(promptLength)]);
@@ -20,8 +21,7 @@ function Prompts(props) {
     const date = `${today.getFullYear()}-${
       today.getMonth() + 1
     }-${today.getDate()}`;
-    const time = `${today.getHours()}:${today.getMinutes()}:${today.getSeconds()}`;
-    return `${date} ${time}`;
+    return `${date}`;
   };
 
   //captures the text input values
@@ -81,117 +81,8 @@ function Prompts(props) {
             value={userInput}
           />
 
-          {/* Moods bar start. user selects a mood to include with thought musing*/}
-          <label htmlFor="moodWrapper">
-            <h4>How are you feeling today?</h4>
-          </label>
+          <Moodbar handleMood={handleMood} />
 
-          <div className="moodWrapper">
-            <label htmlFor="happy" className="visually-hidden">
-              happy mood
-            </label>
-            <input
-              type="image"
-              src="/assets/grin-regular.svg"
-              alt="happy face line drawing"
-              className="mood"
-              onClick={handleMood}
-              value="happy"
-              id="happy"
-            ></input>
-
-            <label htmlFor="love" className="visually-hidden">
-              love mood
-            </label>
-            <input
-              type="image"
-              src="/assets/grin-hearts-regular.svg"
-              alt="happy face with heart eyes line drawing"
-              className="mood"
-              onClick={handleMood}
-              value="love"
-              id="love"
-            ></input>
-
-            <label htmlFor="excited" className="visually-hidden">
-              excited mood
-            </label>
-            <input
-              type="image"
-              src="/assets/grin-stars-regular.svg"
-              alt="excited face line drawing"
-              className="mood"
-              onClick={handleMood}
-              value="excited"
-              id="excited"
-            ></input>
-
-            <label htmlFor="whatever" className="visually-hidden">
-              whatever mood
-            </label>
-            <input
-              type="image"
-              src="/assets/meh-rolling-eyes-regular.svg"
-              alt="indifferent face line drawing"
-              className="mood"
-              onClick={handleMood}
-              value="whatever"
-              id="whatever"
-            ></input>
-
-            <label htmlFor="angry" className="visually-hidden">
-              angry mood
-            </label>
-            <input
-              type="image"
-              src="/assets/angry-regular.svg"
-              alt="angry face line drawing"
-              className="mood"
-              onClick={handleMood}
-              value="angry"
-              id="angry"
-            ></input>
-
-            <label htmlFor="shocked" className="visually-hidden">
-              shocked mood
-            </label>
-            <input
-              type="image"
-              src="/assets/dizzy-regular.svg"
-              alt="shocked face line drawing"
-              className="mood"
-              onClick={handleMood}
-              value="shocked"
-              id="shocked"
-            ></input>
-
-            <label htmlFor="tired" className="visually-hidden">
-              tired mood
-            </label>
-            <input
-              type="image"
-              src="/assets/tired-regular.svg"
-              alt="tired face line drawing"
-              className="mood"
-              onClick={handleMood}
-              value="tired"
-              id="tired"
-            ></input>
-
-            <label htmlFor="sad" className="visually-hidden">
-              sad mood
-            </label>
-            <input
-              type="image"
-              src="/assets/sad-cry-regular.svg"
-              alt="sad face line drawing"
-              className="mood"
-              onClick={handleMood}
-              value="sad"
-              id="sad"
-            ></input>
-          </div>
-          {/* Moods bar end*/}
           <div className="promptBtnContainer">
             <button onClick={handleRandom}>generate a new prompt</button>
             <input type="submit" onClick={handleClick} value="SUBMIT" />
--- src/firebase.js
+++ src/firebase.js
@@ -3,16 +3,14 @@ import "firebase/database";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyATFPZTVHtZ72yo6V2h7PCav3rsd9aiCms",
-  authDomain: "tododanish.firebaseapp.com",
-  databaseURL: "https://tododanish-default-rtdb.firebaseio.com",
-  projectId: "tododanish",
-  storageBucket: "tododanish.appspot.com",
-  messagingSenderId: "738369950073",
-  appId: "1:738369950073:web:7061ebd2e3277215cadfa8",
+  apiKey: "AIzaSyDS6rN5guzn9ZE5Cfqzo2sp3cPRz6KkhdY",
+  authDomain: "humbleponderings.firebaseapp.com",
+  projectId: "humbleponderings",
+  storageBucket: "humbleponderings.appspot.com",
+  messagingSenderId: "450362581872",
+  appId: "1:450362581872:web:8b2d8fbda175cfd7f3a5b5",
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
 export default firebase;
-
